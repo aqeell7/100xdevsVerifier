fda484dfc4fc724cd515a8de44fe66e1e4643726
ditto-chaar <md.aqeelahmed007@gmail.com>
2024-12-20 23:25:09 +0530

    removed redundant function

--- app/error.js
+++ app/error.js
@@ -1,6 +1,6 @@
 'use client'
 
-export default function Error({ error, reset }) {
+export default function Error({ error }) {
   return (
     <div className="grid place-items-center min-h-screen bg-gray-50 text-center p-4">
       <div>
@@ -20,12 +20,7 @@ export default function Error({ error, reset }) {
 
         <h2 className="text-lg font-medium text-red-600 mb-4">Something went wrong</h2>
 
-        <button 
-          onClick={reset}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        >
-          Try again
-        </button>
+       
       </div>
     </div>
   )
